3ccb19e235f1ae15b8258acb0c763f2edeaf2eff
Dawn\Mark <mark@chavfreezone.me.uk>
2017-05-21 16:24:21 +0100

    Added validation layers and debug callbacks and the ext_loader from Vulkan-Docs to handle extension function loading.

--- Leonard/Leonard/HelloTriangleApplication.cpp
+++ Leonard/Leonard/HelloTriangleApplication.cpp
@@ -21,20 +21,62 @@ void HelloTriangleApplication::initWindow()
 void HelloTriangleApplication::initVulkan()
 {
   createInstance();
+  setupDebugCallback();
 }
 
-void HelloTriangleApplication::createInstance()
+bool HelloTriangleApplication::checkValidationLayerSupport()
 {
-  vk::ApplicationInfo appInfo;
-  appInfo.pApplicationName = "Hello Triangle";
-  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.pEngineName = "No Engine"; // Soon tm
-  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.apiVersion = VK_API_VERSION_1_0;
+  uint32_t layerCount;
+  vk::enumerateInstanceLayerProperties(&layerCount, nullptr);
+  std::vector<vk::LayerProperties> availableLayers(layerCount);
+  vk::enumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-  vk::InstanceCreateInfo createInfo;
-  createInfo.pApplicationInfo = &appInfo;
+  // Check the required validation layers against available layers
+  for (const char *layerName : validationLayers)
+  {
+    bool layerFound = false;
 
+    for (const auto &layerProperties : availableLayers)
+    {
+      if (strcmp(layerName, layerProperties.layerName) == 0)
+      {
+        layerFound = true;
+        break;
+      }
+    }
+
+    if (!layerFound)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+std::vector<const char*> HelloTriangleApplication::getRequiredExtensions()
+{
+  std::vector<const char*> extensions;
+
+  unsigned int glfwExtensionCount = 0;
+  const char **glfwExtensions;
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  for (unsigned int i = 0; i < glfwExtensionCount; i++)
+  {
+    extensions.push_back(glfwExtensions[i]);
+  }
+
+  if (enableValidationLayers)
+  {
+    extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+  }
+
+  return extensions;
+}
+
+void HelloTriangleApplication::listAvailableExtensions()
+{
   // Get vulkan extensions
   uint32_t extensionCount = 0;
   vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
@@ -63,17 +105,79 @@ void HelloTriangleApplication::createInstance()
               << ((isReqdByGlfw) ? " (Required by GLFW)" : "")
               << std::endl;
   }
+}
 
-  createInfo.enabledExtensionCount = glfwExtensionCount;
-  createInfo.ppEnabledExtensionNames = glfwExtensions;
+void HelloTriangleApplication::createInstance()
+{
+  if (enableValidationLayers && !checkValidationLayerSupport())
+  {
+    throw std::runtime_error("Validation layers requested, but not available!");
+  }
 
-  createInfo.enabledLayerCount = 0;
+  vk::ApplicationInfo appInfo;
+  appInfo.setPApplicationName("Hello Triangle")
+         .setApplicationVersion(VK_MAKE_VERSION(1, 0, 0))
+         .setPEngineName("No Engine")
+         .setEngineVersion(VK_MAKE_VERSION(1, 0, 0))
+         .setApiVersion(VK_API_VERSION_1_0);
+  //appInfo.pApplicationName = "Hello Triangle";
+  //appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  //appInfo.pEngineName = "No Engine"; // Soon tm
+  //appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  //appInfo.apiVersion = VK_API_VERSION_1_0;
+
+#ifdef _DEBUG
+  listAvailableExtensions();
+#endif
+
+  vk::InstanceCreateInfo createInfo;
+  createInfo.pApplicationInfo = &appInfo;
+
+  auto extensions = getRequiredExtensions();
+
+  createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+  createInfo.ppEnabledExtensionNames = extensions.data();
+
+  if (enableValidationLayers)
+  {
+    createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+    createInfo.ppEnabledLayerNames = validationLayers.data();
+  }
+  else
+  {
+    createInfo.enabledLayerCount = 0;
+  }
 
   vk::Result result = vk::createInstance(&createInfo, nullptr, &instance);
   if (result != vk::Result::eSuccess)
   {
     throw std::runtime_error("Failed to create instance!");
-  }    
+  }
+
+  // Use the ext_loader/vulkan_ext loader to fetch the extension functions which aren't loaded by default
+  // TODO: Consider moving to after device initialisation as we only have one device
+  vkExtInitInstance(instance);
+}
+
+void HelloTriangleApplication::setupDebugCallback()
+{
+  if (!enableValidationLayers) return;
+
+  vk::DebugReportCallbackCreateInfoEXT createInfo;
+  createInfo.setFlags(vk::DebugReportFlagBitsEXT::eError | vk::DebugReportFlagBitsEXT::eWarning)
+            .setPfnCallback(debugCallback);
+
+  if (instance.createDebugReportCallbackEXT(&createInfo, nullptr, &callback) != vk::Result::eSuccess)
+  {
+    throw std::runtime_error("Failed to setup debug callback!");
+  }
+}
+
+void HelloTriangleApplication::removeDebugCallback()
+{
+  if (!enableValidationLayers) return;
+
+  instance.destroyDebugReportCallbackEXT(callback);
 }
 
 void HelloTriangleApplication::mainLoop()
@@ -86,6 +190,7 @@ void HelloTriangleApplication::mainLoop()
 
 void HelloTriangleApplication::cleanup()
 {
+  removeDebugCallback();
   instance.destroy();
 
   glfwDestroyWindow(window);
--- Leonard/Leonard/HelloTriangleApplication.hpp
+++ Leonard/Leonard/HelloTriangleApplication.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vulkan/vulkan.hpp>
+#include <ext_loader\vulkan_ext.h>
 #include <GLFW/glfw3.h> // TODO: Replace GLFW with own window manager
 
 #include <iostream>
@@ -16,7 +17,12 @@ private:
   void initWindow();
 
   void initVulkan();
+  bool checkValidationLayerSupport();
+  std::vector<const char*> getRequiredExtensions();
+  void listAvailableExtensions();
   void createInstance();
+  void setupDebugCallback();
+  void removeDebugCallback();
 
   void mainLoop();
 
@@ -28,4 +34,31 @@ private:
 
   // Vulkan stuff
   vk::Instance instance;
+  vk::DebugReportCallbackEXT callback;
+
+  const std::vector<const char*> validationLayers = 
+  {
+    "VK_LAYER_LUNARG_standard_validation"
+  };
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+  const bool enableValidationLayers = true;
+#endif
+
+  // This callback is the closest to the C-API this program gets, because even the C++-API still falls back on
+  // the C-API's typedefs for function pointers.
+  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+      VkDebugReportFlagsEXT flags
+    , VkDebugReportObjectTypeEXT objType
+    , uint64_t obj
+    , size_t location
+    , int32_t code
+    , const char *layerPrefix
+    , const char *msg
+    , void *userData)
+  {
+    std::cerr << "Validation Layer: " << msg << std::endl;
+    return VK_FALSE;
+  }
 };
